cc2d9117893d6dbf6c97d73969193e8542b390ed
Sander de Leeuw <sander@peakfijn.nl>
2026-08-25 20:22:18 +0200

    feat: convert to async views

--- src/contacts/api/contact.py
+++ src/contacts/api/contact.py
@@ -8,21 +8,31 @@ from contacts.schemas import ContactCreateUpdateSchema, ContactSchema
 router = Router(tags=["contacts"])
 
 
+def get_queryset(request: HttpRequest):
+    user = request.auth
+
+    if request.method in ("PUT", "PATCH", "DELETE"):
+        return Contact.objects.writable_for_user(user)
+    else:
+        return Contact.objects.readable_for_user(user)
+
+
+def get_object(request: HttpRequest, pk: int):
+    return get_object_or_404(get_queryset(request), pk=pk)
+
+
 @router.get("/", response=list[ContactSchema])
-def list_contacts(request: HttpRequest):
-    return Contact.objects.readable_for_user(request.auth)
+async def list_contacts(request: HttpRequest):
+    return get_queryset(request)
 
 
 @router.get("/{contact_id}/", response=ContactSchema)
-def get_contact(request: HttpRequest, contact_id: int):
-    return get_object_or_404(
-        Contact.objects.readable_for_user(request.auth),
-        id=contact_id,
-    )
+async def get_contact(request: HttpRequest, contact_id: int):
+    return get_object(request, contact_id)
 
 
 @router.post("/", response={201: ContactSchema})
-def create_contact(request: HttpRequest, payload: ContactCreateUpdateSchema):
+async def create_contact(request: HttpRequest, payload: ContactCreateUpdateSchema):
     return 201, Contact.objects.create(
         owner=request.auth,
         **payload.dict(),
@@ -30,15 +40,12 @@ def create_contact(request: HttpRequest, payload: ContactCreateUpdateSchema):
 
 
 @router.patch("/{contact_id}/", response=ContactSchema)
-def update_contact(
+async def update_contact(
     request: HttpRequest,
     contact_id: int,
     payload: PatchDict[ContactCreateUpdateSchema],
 ):
-    contact = get_object_or_404(
-        Contact.objects.writable_for_user(request.auth),
-        id=contact_id,
-    )
+    contact = get_object(request, contact_id)
 
     for attr, value in payload.items():
         setattr(contact, attr, value)
@@ -49,11 +56,8 @@ def update_contact(
 
 
 @router.delete("/{contact_id}/", response={204: None})
-def delete_contact(request: HttpRequest, contact_id: int):
-    contact = get_object_or_404(
-        Contact.objects.writable_for_user(request.auth),
-        id=contact_id,
-    )
+async def delete_contact(request: HttpRequest, contact_id: int):
+    contact = get_object(request, contact_id)
     contact.delete()
 
     return 204, None
